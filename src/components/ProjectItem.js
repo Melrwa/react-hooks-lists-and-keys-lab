ce8425341eb96c8c77731325c2cc995ea243601b
melrwa <orwamelkizedek@gmail.com>
2024-10-31 18:30:18 +0300

    complete lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
+
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
   return (
     <div className="project-item">
       <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      <h4>{about}</h4>
+      <div>
+        {technologies.map((tech, index) => (
+          <span className="technologies" key={index}>
+            {tech}
+          </span>
+        ))}
       </div>
     </div>
   );
